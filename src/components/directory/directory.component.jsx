b35af39288c61854782d67a6fabd6d3abdec06c9
betulyanal1@gmail.com <betulyanal1@gmail.com>
2022-06-17 14:27:32 +0300

    routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
 import CategoryItem from "../category-item/category-item.component";
 import "./directory.component.scss";
+
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
